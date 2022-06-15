4032a1951a0398402933b2abe3f5b30465f0178d
Nearchos Paspallis <nearchos@aspectsense.com>
2022-06-15 09:06:28 +0300

    Added GMaps key for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBIMG--lOhG4-KVkIhp44CJElpbwiHpXBc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
